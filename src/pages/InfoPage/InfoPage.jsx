bd704d3155ffd501af21e6792f492f70caad7ad4
Makspsh <109795770+Makspsh@users.noreply.github.com>
2025-11-04 09:31:58 +0300

    add HomePage btn in info-page

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./InfoPage.css";
+import homePageIcon from "../../assets/icons/homepage-icon.svg";
 
 export default function InfoPage({ onBack }) {
   return (
     <div className="info-container">
       <h2 className="info-title">Справочник</h2>
       <p className="info-text">Здесь будут карточки</p>
-      <button onClick={onBack}>Назад</button>
+      <button className="home-btn blue" onClick={onBack}>
+        <img src={homePageIcon} alt="home" />
+      </button>
     </div>
   );
 }
