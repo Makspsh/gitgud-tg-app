943f4dffbf2efb79be08256b0abbf53152042928
Makspsh <109795770+Makspsh@users.noreply.github.com>
2025-11-04 08:19:17 +0300

    fix structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import StartMenu from "./pages/StartMenu"
+import StartMenu from "./pages/StartMenu/StartMenu"
 const tg = window.Telegram.WebApp;
 
 function App() {
---
+++ src/pages/StartMenu/StartMenu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./StartMenu.css";
+import infoIcon from "../../assets/icons/info-icon.svg";
+import questionIcon from "../../assets/icons/question-icon.svg";
+import calculatorIcon from "../../assets/icons/calculate-icon.svg";
+
+export default function StartMenu() {
+  return (
+    <div className="menu-container">
+      <h1 className="menu-title">разделы</h1>
+      <div className="menu-buttons">
+        <button className="menu-btn blue">
+          <img src={infoIcon} alt="info" />
+        </button>
+        <button className="menu-btn green">
+          <img src={questionIcon} alt="question" />
+        </button>
+        <button className="menu-btn darkgreen">
+          <img src={calculatorIcon} alt="calculator" />
+        </button>
+      </div>
+    </div>
+  );
+}
